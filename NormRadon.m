c6121f313195e8e291ddf2e3e6006ebd48d05b5c
fqjin <fqjin@users.noreply.github.com>
2022-03-13 21:18:39 -0400

    Improve NormRadon.m docstring

--- NormRadon.m
+++ NormRadon.m
@@ -1,23 +1,28 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Normalized radon transform
-% Version 1.1, Felix Jin 2021, felix.jin@duke.edu
+% Perform normalized Radon transform
 %
-% Radon transform of data is divided by the radon
-% transform of a ones matrix or input data mask.
+% To account for varying lengths of trajectories, the Radon
+% transform of the data is divided by the Radon transform of
+% a ones matrix. When an optional mask is used over the data,
+% the ones matrix is also masked.
+%
+% radon_ones can be pre-calculated and provided to avoid repeat
+% calculation when processing multiple data with the same mask.
 %
 % Arguments:
 %   data (matrix)
 %   theta (vector)
-%   radon_ones(matrix): will be calculated if not provided
+%   radon_ones (matrix): will be calculated if not provided
 %   mask (matrix): multiplies data by mask and calculates radon_ones
-%   epsilon (scalar): Threshold below which radon_ones will be set NaN
-%       This suppresses short length trajectories.
+%   epsilon (scalar): Threshold below which radon_ones will be set
+%     to NaN, which suppresses short length trajectories. Default
+%     is 0.1, meaning trajectories < 10% of max length are ignored.
 %
 % Returns:
-%   radout.txfm (matrix)
+%   radout.txfm (matrix): the normalized Radon transform
 %   radout.theta (vector)
 %   radout.rp (vector)
-%   radout.radon_ones (matrix)
+%   radout.radon_ones (matrix): the normalizing constant
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function radout = NormRadon(data, theta, radon_ones, mask, epsilon)
 if ~exist('epsilon', 'var')
