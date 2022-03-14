1586571096455654d501aae910e92beaac5109e0
fqjin <fqjin@users.noreply.github.com>
2022-03-13 23:13:27 -0400

    Use tiledlayout in PlotRadon.m

--- PlotRadon.m
+++ PlotRadon.m
@@ -15,11 +15,11 @@ if fignum > 0
     figure(fignum)
     clf
 end
-% Versions before 2019a cannot use hex colors. Crazy!
-blue = [0 0.4470 0.7410];
-oran = [0.8500 0.3250 0.0980];
+blue = [0, 0.4470, 0.7410];
+oran = [0.8500, 0.3250, 0.0980];
 
-subplot(3,5,[1,2,6,7])
+tiledlayout(3, 5, 'TileSpacing', 'none')
+nexttile(1, [2, 2])
 imagesc(data.tMsec, data.xMm, data.data)
 hold on
 plot(p_out.times, data.xMm, 'k', 'LineWidth', 2)
@@ -28,13 +28,14 @@ plot(t_out.times, data.xMm, 'w', 'LineWidth', 2)
 end
 xlabel('time (ms)');
 ylabel('lateral (mm)');
+colorbar
 if ~exist('t_out', 'var')
 title(sprintf('Speed = %.2f m/s', p_out.speed))
 else
 title(sprintf('Peak=%.2f m/s , Trough=%.2f m/s', p_out.speed, t_out.speed))
 end
 
-subplot(3,5,[3,4,8,9])
+nexttile(3, [2, 2])
 imagesc(radout.theta, radout.rp, radout.txfm)
 hold on
 plot(peak.angle, peak.rad_pos, '.k', 'MarkerSize', 12)
@@ -42,10 +43,9 @@ if exist('trough', 'var')
 plot(trough.angle, trough.rad_pos, '.w', 'MarkerSize', 12)
 end
 title('Radon Transform')
-xticks([])
-yticks([])
+set(gca, 'YAxisLocation', 'right')
 
-subplot(3,5,[5,10])
+nexttile(5, [2, 1])
 max_line = max(radout.txfm, [], 2);
 plot(max_line, radout.rp, 'Color',blue,'LineWidth',1)
 hold on
@@ -57,12 +57,12 @@ plot([0, 0], radout.rp([1, end]), 'k')
 plot(min(radout.txfm, [], 2), radout.rp, 'Color',oran,'LineWidth',1)
 plot([-trough.max_val, 0], [trough.rad_pos, trough.rad_pos], '--','Color',oran,'LineWidth',1)
 end
+ylim(radout.rp([1,end]))
 set(gca, 'YDir', 'reverse')
-set(gca, 'YAxisLocation', 'right')
+set(gca,'Yticklabel',[])
 ylabel('radial position')
-axis tight
 
-subplot(3,5,[13,14])
+nexttile(13, [1, 2])
 max_line = max(radout.txfm, [], 1);
 plot(radout.theta, max_line, 'Color',blue,'LineWidth',1)
 hold on
@@ -74,7 +74,9 @@ plot(radout.theta([1, end]), [0, 0], 'k')
 plot(radout.theta, min(radout.txfm, [], 1), 'Color',oran,'LineWidth',1)
 plot([trough.angle, trough.angle], [-trough.max_val, 0], '--','Color',oran,'LineWidth',1)
 end
+xlim(radout.theta([1,end]))
+set(gca, 'XAxisLocation', 'top')
+set(gca,'Xticklabel',[])
 xlabel('theta (deg)')
-axis tight
 
 end
