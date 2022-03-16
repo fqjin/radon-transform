852ec8d6f7b0825f35a5656c00ade063c3ab78fc
fqjin <fqjin@users.noreply.github.com>
2022-03-16 01:06:25 -0400

    Remove zero line from PlotRadon.m
    If data is not zero-centered, the peak and trough lines will not meet
    up at zero. This also simplifies plotting the vertical dotted line.

--- PlotRadon.m
+++ PlotRadon.m
@@ -49,13 +49,11 @@ nexttile(5, [2, 1])
 max_line = max(radout.txfm, [], 2);
 plot(max_line, radout.rp, 'Color',blue,'LineWidth',1)
 hold on
-if ~exist('trough', 'var')
 plot([min(max_line), peak.max_val], [peak.rad_pos, peak.rad_pos], '--','Color',blue,'LineWidth',1)
-else
-plot([0, peak.max_val], [peak.rad_pos, peak.rad_pos], '--','Color',blue,'LineWidth',1)
-plot([0, 0], radout.rp([1, end]), 'k')
-plot(min(radout.txfm, [], 2), radout.rp, 'Color',oran,'LineWidth',1)
-plot([-trough.max_val, 0], [trough.rad_pos, trough.rad_pos], '--','Color',oran,'LineWidth',1)
+if exist('trough', 'var')
+min_line = min(radout.txfm, [], 2);
+plot(min_line, radout.rp, 'Color',oran,'LineWidth',1)
+plot([-trough.max_val, max(min_line)], [trough.rad_pos, trough.rad_pos], '--','Color',oran,'LineWidth',1)
 end
 ylim(radout.rp([1,end]))
 set(gca, 'YDir', 'reverse')
@@ -66,13 +64,11 @@ nexttile(13, [1, 2])
 max_line = max(radout.txfm, [], 1);
 plot(radout.theta, max_line, 'Color',blue,'LineWidth',1)
 hold on
-if ~exist('trough', 'var')
 plot([peak.angle, peak.angle], [min(max_line), peak.max_val], '--','Color',blue,'LineWidth',1)
-else
-plot([peak.angle, peak.angle], [0, peak.max_val], '--','Color',blue,'LineWidth',1)
-plot(radout.theta([1, end]), [0, 0], 'k')
+if exist('trough', 'var')
+min_line = min(radout.txfm, [], 1);
 plot(radout.theta, min(radout.txfm, [], 1), 'Color',oran,'LineWidth',1)
-plot([trough.angle, trough.angle], [-trough.max_val, 0], '--','Color',oran,'LineWidth',1)
+plot([trough.angle, trough.angle], [-trough.max_val, max(min_line)], '--','Color',oran,'LineWidth',1)
 end
 xlim(radout.theta([1,end]))
 set(gca, 'XAxisLocation', 'top')
